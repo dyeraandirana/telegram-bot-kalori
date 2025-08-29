1a33de9ee63ae81aa94d98b7c7d3218307c01535
dyeraandirana <135441436+dyeraandirana@users.noreply.github.com>
2025-08-29 12:33:27 +0700

    Update8 webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -37,7 +37,7 @@ Saya mengunggah sebuah foto makanan. Tolong lakukan analisis:
   `;
 
   const response = await fetch(
-    `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${GEMINI_API_KEY}`,
+    `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${GEMINI_API_KEY}`,
     {
       method: "POST",
       headers: { "Content-Type": "application/json" },
